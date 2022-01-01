0dca01f8b161da584a07bc3415a019c8e4703278
Md. Riyasat Hossain <riyasat.hossain.29@gmail.com>
2022-01-01 17:58:10 +0600

    Basic features added

---
+++ README.md
@@ -0,0 +1,5 @@
+# JSON-DB
+Handler for local JSON (NoSQL) Databases with data sending, receiving, updating, deleting, and even with extra ".on" events!
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,30 +1,89 @@
 import fs from "fs";
-import path from "path";
+// import path from "path";
 import EventEmitter from "events";
 
-export default class JSON_DB {
+class JSON_DB extends EventEmitter {
   constructor(dbFilePath) {
-    this.status = new EventEmitter();
+    super();
+
     this.dbFilePath = dbFilePath;
-    this.db = {};
-    this.loadDB();
+    this.snapshot = this.loadDB();
   }
 
   loadDB() {
+    let dbData;
+
     if (fs.existsSync(this.dbFilePath)) {
-      this.db = JSON.parse(fs.readFileSync(this.dbFilePath));
+      // Check if the file is completely empty
+      if (fs.readFileSync(this.dbFilePath).toString() === "") {
+        fs.writeFileSync(this.dbFilePath, "{}");
+      }
+
+      dbData = JSON.parse(fs.readFileSync(this.dbFilePath));
+
+      // console.log(dbData)
     } else {
       throw new Error(`JSON file not found: ${this.dbFilePath}`);
     }
 
-    return this;
+    return dbData;
   }
 
-  push(id, data) {
-    this.db[id] = data;
+  post(id, data) {
+    // Check if id exists
+    if (this.snapshot[id]) {
+      throw new Error(
+        `ID already exists: ${id}\nTry using put() method to update that ID with data instead.`
+      );
+    } else {
+      this.snapshot[id] = data;
 
-    fs.writeFileSync(this.dbFilePath, JSON.stringify(this.db));
+      fs.writeFileSync(this.dbFilePath, JSON.stringify(this.snapshot));
 
-    this.status.emit("push", this.db, id, data);
+      this.emit("add", this.snapshot, id, data);
+    }
+  }
+
+  delete(id) {
+    // Check if id exists
+    if (this.snapshot[id]) {
+      const dataDeleted = this.snapshot[id];
+      delete this.snapshot[id];
+
+      fs.writeFileSync(this.dbFilePath, JSON.stringify(this.snapshot));
+
+      this.emit("delete", this.snapshot, id, dataDeleted);
+    } else {
+      throw new Error(`ID not found: ${id}\nMake sure that ID exists.`);
+    }
+  }
+
+  put(id, data) {
+    // Check if id exists
+    if (this.snapshot[id]) {
+      const prevData = this.snapshot[id];
+      this.snapshot[id] = { ...this.snapshot[id], ...data };
+
+      fs.writeFileSync(this.dbFilePath, JSON.stringify(this.snapshot));
+
+      this.emit("update", this.snapshot, id, prevData, dataUpdated);
+    } else {
+      throw new Error(
+        `ID not found: ${id}\nTry using post() method to add that ID with data instead.`
+      );
+    }
+  }
+
+  get(id) {
+    // Check if id exists
+    if (this.snapshot[id]) {
+      return this.snapshot[id];
+    } else {
+      throw new Error(
+        `ID not found: ${id}\nMake sure you know what you are looking for.`
+      );
+    }
   }
 }
+
+export default JSON_DB;
